8a3864facf58a381c39294ac951d271ca51b1a8b
Mike De Haan <mdehaan@psclistens.com>
2013-07-02 16:35:01 -0500

    Fixed JavaBean and batch file.
    Also fixed commons-lang dependency

--- bin/gru.bat
+++ bin/gru.bat
@@ -1,21 +1,25 @@
 @echo off
+setlocal
 
-set GRU_CP=%GRU_HOME%\lib\ST4-4.0.7.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\antlr-2.7.7.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\antlr-java-parser-1.0.12.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\antlr-runtime-3.5.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\antlr4-4.0.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\antlr4-runtime-4.0.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\asm-4.0.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\asm-analysis-4.0.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\asm-commons-4.0.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\asm-tree-4.0.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\asm-util-4.0.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\commons-io-2.1.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\groovy-2.1.5.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\gru-0.0.1.BUILD-SNAPSHOT.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\org.abego.treelayout.core-1.0.1.jar;%GRU_CP%
-set GRU_CP=%GRU_HOME%\lib\stringtemplate-3.2.1.jar;%GRU_CP%
+set CLASSPATH=%GRU_HOME%\lib\ST4-4.0.7.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\antlr-2.7.7.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\antlr-java-parser-1.0.12.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\antlr-runtime-3.5.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\antlr4-4.0.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\antlr4-runtime-4.0.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\asm-4.0.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\asm-analysis-4.0.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\asm-commons-4.0.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\asm-tree-4.0.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\asm-util-4.0.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\commons-io-2.1.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\commons-io-2.1.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\groovy-2.1.5.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\gru-0.0.1.BUILD-SNAPSHOT.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\org.abego.treelayout.core-1.0.1.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\stringtemplate-3.2.1.jar;%CLASSPATH%
+set CLASSPATH=%GRU_HOME%\lib\commons-lang-2.4.jar;%CLASSPATH%
 
-java -cp %GRU_CP% com.github.gru.GruMain
-:end
+java com.github.gru.GruMain
+
+endlocal
